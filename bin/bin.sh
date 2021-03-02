9f6c62bd86d681a7064ddbc8d8b512c7b7b91663
YAWAsau <62833322+YAWAsau@users.noreply.github.com>
2021-03-02 14:44:27 +0800

    修復環境目錄文件缺少卻繼續執行腳本的缺少二進制error

--- bin/bin.sh
+++ bin/bin.sh
@@ -1,54 +1,46 @@
-#設置二進制命令目錄位置
-filepath=/data/backup_script/bin
-
-busybox_file () {
-    busybox="$filepath/busybox"
-    if [[ -e $busybox ]]; then
-        for a in $($busybox --list) ; do
-            if [[ -n $a ]]; then
-                if [[ -d $filepath ]]; then
-                    [[ ! -e $filepath/$a ]] && ln -s $busybox "$filepath/$a"
-                fi    
-            fi    
-        done
-        #如果存在busybox 則創建私有目錄並且移動所需二進制後刪除當前$PATH
-        export PATH=$filepath    
-    else 
-        echo "錯誤 缺少$busybox"
-        exit 1
-    fi
-    [[ ! $PATH == $filepath ]] && echo "環境變量位置錯誤" && exit 1
-    echo "環境變數: $PATH"
-}
 
 #補上遺失指令集
 Add_path () {
-    if [[ ! -e $filepath/$1 ]]; then
-        if [[ -e $2/$1 ]]; then
-            if [[ $3 == n ]]; then                
-                if [[ -d $filepath ]]; then
-                    cp -r $2/$1 $filepath
-                    chmod 0777 $filepath/$1
-                    busybox_file
-                else
-                    mkdir -p $filepath
-                    cp -r $2/$1 $filepath
-                    chmod 0777 $filepath/$1
-                    busybox_file
+    #設置二進制命令目錄位置
+    filepath=/data/backup_tools
+    busybox="$filepath/busybox"
+    #工具絕對位置
+    tools_path=${0%/*}/tools
+    if [[ -e $busybox ]]; then        
+        if [[ ! -e $filepath/$1 ]]; then
+            if [[ $2 == n ]]; then             
+                if [[ -e $tools_path/$1 ]]; then                   
+                    cp -r $tools_path/$1 $filepath
+                    chmod 0777 $filepath/$1                                           
+                else 
+                    echo "$tools_path/$1不存在 腳本所需的$1缺少"
+                    exit 1
                 fi
             else
-                if [[ -d $filepath ]]; then
-                    ln -s $2/$1 $filepath
+                if [[ -e $3/$1 ]]; then
+                    ln -s $3/$1 $filepath
                 else
-                    mkdir -p $filepath
-                    ln -s $2/$1 $filepath
-                fi
-            fi
-        else 
-            echo "$2/$1不存在 腳本所需的$1缺少"
+                    echo "錯誤: $3/$1不存在"
+                    exit 1
+                fi                      
+            fi        
+        fi    
+        [[ ! -e $filepath/$1 ]] && echo "錯誤: $filepath/$1不存在" && exit 1
+        export PATH=$filepath
+        [[ ! $PATH == $filepath ]] && echo "環境變量位置錯誤" && exit 1        
+    else
+        if [[ -e $tools_path/busybox ]]; then
+            [[ ! -d $filepath ]] && mkdir -p $filepath
+            cp -r $tools_path/busybox $busybox
+            chmod 0777 $busybox
+            for a in $($busybox --list) ; do
+                if [[ -n $a ]]; then                    
+                    [[ ! -e $filepath/$a ]] && ln -s $busybox "$filepath/$a"                    
+                fi    
+            done
+        else
+            echo "錯誤 缺少$tools_path/busybox"
             exit 1
         fi
-    else
-        [[ $filepath/$1 == $filepath/busybox ]] && busybox_file
-    fi    
+    fi        
 }
