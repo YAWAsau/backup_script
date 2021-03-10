cdd1bed87ce349d6b2ab7f6cbdc22f342b2513f4
YAWAsau <62833322+YAWAsau@users.noreply.github.com>
2021-03-10 21:01:39 +0800

    Add files via upload
    將語言顯示由繁體>簡體迎合語言習慣

--- appname.sh
+++ appname.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
-[[ $(id -u) -ne 0 ]] && echo "你是憨批？不給Root用你媽 爬" && exit 1
-[[ -z $(echo ${0%/*} | grep -v 'mt') ]] && echo "草泥馬不解壓縮？用毛線 憨批" && exit 1
-[[ ! -d ${0%/*}/tools ]] && echo "${0%/*}/tools目錄遺失" && exit 1
-#鏈接腳本設置環境變量
+[[ $(id -u) -ne 0 ]] && echo "你是憨批？不给Root用你妈 爬" && exit 1
+[[ -z $(echo ${0%/*} | grep -v 'mt') ]] && echo "草泥马不解压缩？用毛缐 憨批" && exit 1
+[[ ! -d ${0%/*}/tools ]] && echo "${0%/*}/tools目录遗失" && exit 1
+#链接脚本设置环境变量
 . ${0%/*}/tools/bin.sh
-#設置命令和目錄位置及是否使用鏈接方式
+#设置命令和目录位置及是否使用链接方式
 tools_path=${0%/*}/tools
 Add_path
 if [[ -d /system/bin ]]; then
@@ -20,17 +20,17 @@ Add_path "zip" n
 Add_path "pm" y $system_path
 Add_path "cmd" y $system_path 
 Add_path "am" y $system_path 
-echo "環境變數: $PATH"
+echo "环境变数: $PATH"
 
 name=$(pm list packages -3 | sed 's/package://g' | grep -v 'xiaomi' | grep -v 'miui')
 sys=$(pm list packages | egrep 'com.android.chrome|com.google.android.inputmethod.latin|com.digibites.accubattery' | sed 's/package://g')
-echo "#不需要恢復還原的應用請在開頭注釋# 比如#xxxxxxxx 酷安">${0%/*}/Apkname.txt
-echo "請勿關閉腳本，等待提示結束"
+echo "#不需要恢復还原的应用请在开头注释# 比如#xxxxxxxx 酷安">${0%/*}/Apkname.txt
+echo "请勿关闭脚本，等待提示结束"
 for name in $name $sys; do
-    #獲取apk中文名稱         
+    #获取apk中文名称         
     Appname1=$(aapt dump badging $(pm path "$name" | cut -f2 -d ':') | grep -w "application-label-zh-CN" | head -1 | sed "s/.*:\'//g" | sed "s/\'//g" | sed 's/ //g')                                
     Appname2=$(aapt dump badging $(pm path "$name" | cut -f2 -d ':') | grep -w "application-label-zh-TW:" | sed 's/application-label-zh-TW://g' | sed "s/\'//g" | sed 's/ //g')
-    #獲取apk默認名稱
+    #获取apk默认名称
     Appname3=$(aapt dump badging $(pm path "$name" | cut -f2 -d ':') | grep -w "application-label:" | sed 's/application-label://g' | sed "s/\'//g" | sed 's/ //g')
     [[ ! $(echo $Appname1 | wc -l) == 0  ]] && Appname=$Appname1
     [[ -z $Appname ]] && Appname=$Appname2
@@ -38,5 +38,5 @@ for name in $name $sys; do
     [[ -z $Appname ]] && Appname=$name
     [[ -z $(cat ${0%/*}/Apkname.txt | grep -v "#" | sed -e '/^$/d' | grep -w "$name") ]] && echo "$Appname $name">>${0%/*}/Apkname.txt
 done
-echo "如果執行中出現AndroidManifest.xml:XX: error: ERROR 代表dump名稱錯誤 以使用包名替代，不影響備份"
-echo "輸出包名結束 請查看${0%/*}/Apkname.txt"
+echo "如果执行中出现AndroidManifest.xml:XX: error: ERROR 代表dump名称错误 以使用包名替代，不影响备份"
+echo "输出包名结束 请查看${0%/*}/Apkname.txt"
