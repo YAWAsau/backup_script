6d5eb1314aeb0a48e9972bc50221435f120274cf
YAWAsau <229953100a@gmail.com>
2021-03-19 12:24:31 +0800

    增加架構判斷 強制使用script自帶busybox

--- tools/bin.sh
+++ tools/bin.sh
@@ -1,10 +1,16 @@
 
-#補上遺失指令集
+#补上遗失指令集
 Add_path () {
-    #設置二進制命令目錄位置
+    #设置二进制命令目录位置
     filepath=/data/backup_tools
     busybox="$filepath/busybox"
-    #工具絕對位置  
+    if [[ -e $busybox ]]; then
+        if [[ ! $(du -k -s $busybox | awk '{print $1}') == $(du -k -s $tools_path/busybox-arm64 | awk '{print $1}') ]]; then
+            rm -rf $filepath
+            echo "busybox大小不一致，已删除并且重新设置"
+        fi
+    fi   
+    #工具绝对位置  
     if [[ -e $busybox ]]; then        
         if [[ ! -e $filepath/$1 ]]; then
             if [[ $2 == n ]]; then             
@@ -12,28 +18,31 @@ Add_path () {
                     cp -r $tools_path/$1 $filepath
                     chmod 0777 $filepath/$1                                           
                 else 
-                    echo "$tools_path/$1不存在 腳本所需的$1缺少"
+                    echo "$tools_path/$1不存在 脚本所需的$1缺少"
                     exit 1
                 fi
             else
                 if [[ -e $3/$1 ]]; then
                     ln -s $3/$1 $filepath
                 else
-                    echo "錯誤: $3/$1不存在"
+                    echo "错误: $3/$1不存在"
                     exit 1
                 fi                      
             fi        
         fi    
-        [[ ! -e $filepath/$1 ]] && echo "錯誤: $filepath/$1不存在" && exit 1
+        [[ ! -e $filepath/$1 ]] && echo "错误: $filepath/$1不存在" && exit 1
         export PATH=$filepath
-        [[ ! $PATH == $filepath ]] && echo "環境變量位置錯誤" && exit 1        
-    else
-        #判斷是否存在Magisk busybox 則優先使用，如沒有使用腳本自身busybox
-        if [[ -e /data/adb/magisk/busybox ]]; then    
-            echo "存在Magisk busybox path:/data/adb/magisk/busybox"
-            echo "優先使用 佈置環境中....."
+        [[ ! $PATH == $filepath ]] && echo "环境变量位置错误" && exit 1        
+    else        
+        echo "不存在$busybox 设置环境中...."
+        abi=$(getprop ro.product.cpu.abi)
+        case $abi in
+        arm64*) echo "设备架构$abi" ;;    
+        *) echo "未知的架构: $abi"; exit 1 ;;
+        esac
+        if [[ -e $tools_path/busybox-arm64 ]]; then
             [[ ! -d $filepath ]] && mkdir -p $filepath
-            cp -r /data/adb/magisk/busybox $busybox
+            cp -r $tools_path/busybox-arm64 $busybox
             chmod 0777 $busybox
             for a in $($busybox --list) ; do
                 if [[ -n $a ]]; then                    
@@ -41,25 +50,10 @@ Add_path () {
                 fi    
             done
             export PATH=$filepath
-            [[ ! $PATH == $filepath ]] && echo "環境變量位置錯誤" && exit 1         
+            [[ ! $PATH == $filepath ]] && echo "环境变量位置错误" && exit 1        
         else
-            if [[ -e $tools_path/busybox ]]; then
-                echo "不存在Magisk busybox"
-                echo "使用$tools_path/busybox 佈置環境中....."
-                [[ ! -d $filepath ]] && mkdir -p $filepath
-                cp -r $tools_path/busybox $busybox
-                chmod 0777 $busybox
-                for a in $($busybox --list) ; do
-                    if [[ -n $a ]]; then                    
-                        [[ ! -e $filepath/$a ]] && ln -s $busybox "$filepath/$a"                    
-                    fi    
-                done
-                export PATH=$filepath
-                [[ ! $PATH == $filepath ]] && echo "環境變量位置錯誤" && exit 1        
-            else
-                echo "錯誤 缺少$tools_path/busybox"
-                exit 1
-            fi
+            echo "错误 缺少$tools_path/busybox-arm64"
+            exit 1
         fi
     fi        
 }
