40abdaeb9047b877014e0dc4a5ef448d343ebb87
YAWAsau <62833322+YAWAsau@users.noreply.github.com>
2021-07-02 19:45:02 +0800

    遺留aapt代碼更正

--- appname.sh
+++ appname.sh
@@ -10,14 +10,14 @@ com.google.android.apps.messaging
 com.digibites.accubattery
 com.google.android.inputmethod.latin
 com.android.chrome"
-name=$(appinfo -o an,pn -d ⎈ -n $system -3 | sed 's/ //g' | sed 's/⎈/ /g')
+
 echo "#不需要恢復还原的应用请在开头注释# 比如#xxxxxxxx 酷安" >${0%/*}/Apkname.txt
 echo "请勿关闭脚本，等待提示结束"
 #删除遗留，防止上次意外中断脚本残留的打印包名文件
 [[ -e ${0%/*}/tools/tmp ]] && rm -rf ${0%/*}/tools/tmp
 echoRgb() {
 	if [[ -n $2 ]]; then
-		if [[ $3 = 1 ]]; then
+		if [[ $3 == 1 ]]; then
 			echo -e "\e[1;32m $1\e[0m"
 		else
 			echo -e "\e[1;31m $1\e[0m"
@@ -28,18 +28,14 @@ echoRgb() {
 }
 i=1
 bn=37
-echo "$name" | while read name; do
+appinfo -d " " -o ands,pn -pn $system -3 | while read name; do
 	[[ $bn -ge 37 ]] && bn=31
 	echoRgb "$i.$name"
 	[[ -z $(cat ${0%/*}/Apkname.txt | grep -v "#" | sed -e '/^$/d' | grep -w "$name") ]] && echo "$name" >>${0%/*}/tools/tmp
 	let i++
 	let bn++
 done
-#dump_name "$g"
-echo "如果执行中出现AndroidManifest.xml:XX: error: ERROR 代表dump名称错误 已使用包名替代，不影响备份"
 echo "整理排列中........"
-sort ${0%/*}/tools/tmp | while read o; do
-	echo $o >>${0%/*}/Apkname.txt
-done
+sort ${0%/*}/tools/tmp>>${0%/*}/Apkname.txt
 rm -rf ${0%/*}/tools/tmp
 echo "输出包名结束 请查看${0%/*}/Apkname.txt"
