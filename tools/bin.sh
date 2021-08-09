f910950cb80002527f298e110770bbb8422c1958
YAWAsau <62833322+YAWAsau@users.noreply.github.com>
2021-08-09 20:25:20 +0800

    Add files via upload

--- tools/bin.sh
+++ tools/bin.sh
@@ -1,21 +1,30 @@
 abi=$(getprop ro.product.cpu.abi)
 case $abi in
-arm64*) echo "設備架構$abi" ;;
+arm64*) 
+	;;
 *)
-	echo "未知的架構: $abi"
+	echo "-未知的架構: $abi"
 	exit 1
 	;;
 esac
 
 #設置二進制命令目錄位置
-[[ -z $tools_path ]] && tools_path=${0%/*}/tools
-#filepath=/data/aosp_tools
+[[ -z $tools_path ]] && echo "未正確指定bin.sh位置" && exit 2
 filepath=/data/backup_tools
 #排除自身
 exclude="
 restore
 busybox_path
 bin.sh"
+MD5() {
+	file_path=$(find $md5path -name "$1" -maxdepth 2 -type f)
+	if [[ -f $file_path ]]; then
+		if [[ ! $(echo $file_path | xargs md5sum | cut -d" " -f1) = $2 ]]; then
+			echo "$1文件被更改或是損毀"
+			exit 1
+		fi
+	fi
+}
 rm_busyPATH() {
 if [[ ! -d $filepath ]]; then
 	mkdir -p $filepath
@@ -37,10 +46,10 @@ if [[ -d $tools_path ]]; then
 	[[ ! -e $tools_path/busybox ]] && echo "$tools_path/busybox不存在" && exit 1
 	busybox="$filepath/busybox"
 	if [[ -e $busybox ]]; then
-		filesize=$(ls -l $busybox | awk '{print $5}')
-		filesize2=$(ls -l $tools_path/busybox | awk '{print $5}')
-		if [[ ! $filesize = $filesize2 ]]; then
-			echo "busybox大小不一致 重新創立環境中"
+		filemd5=$(md5sum $busybox | cut -d" " -f1)
+		filemd5_1=$(md5sum $tools_path/busybox | cut -d" " -f1)
+		if [[ ! $filemd5 = $filemd5_1 ]]; then
+			echo "busybox md5不一致 重新創立環境中"
 			rm -rf $filepath
 			[[ ! -d $filepath ]] && mkdir -p $filepath && echo "設置busybox環境中"
 			rm_busyPATH
@@ -58,18 +67,18 @@ if [[ -d $tools_path ]]; then
 					date|tar) ;;
 					*)
 						if [[ ! -e $filepath/$a ]]; then
-							echo "$a > $filepath/$a"
 							ln -s $busybox "$filepath/$a"
 						fi
 					;;
 					esac
 				done
+				echo "busybox設置完成"
 			fi
 		else
-			filesize=$(ls -l $filepath/$i | awk '{print $5}')
-			filesize2=$(ls -l $tools_path/$i | awk '{print $5}')
-			if [[ ! $filesize = $filesize2 ]]; then
-				echo "$i大小不一致 重新創建"
+			filemd5=$(md5sum $filepath/$i | cut -d" " -f1)
+			filemd5_1=$(md5sum $tools_path/$i | cut -d" " -f1)
+			if [[ ! $filemd5 = $filemd5_1 ]]; then
+				echo "$i md5不一致 重新創建"
 				echo "$i > $filepath/$i"
 				rm -rf $filepath/$i
 				cp -r $tools_path/$i $filepath
@@ -82,11 +91,43 @@ else
 	exit 1
 fi
 #工具絕對位置
-if [[ -e $busybox ]]; then
-	export PATH=$filepath:$PATH
-	echo "环境变数: $PATH"
-	echo "version:$(busybox | head -1)"
-else
-	echo "不存在$busybox ...."
-	exit 1
-fi
+[[ ! -e $busybox ]] && {
+echo "不存在$busybox ...."
+exit 1
+}
+export PATH=$filepath:$PATH
+echo "-環境變數: $PATH"
+echo "-version:$(busybox | head -1 | awk '{print $2}')"
+echo "-設備架構$abi"
+echo "-品牌:$(getprop ro.product.brand)"
+echo "-設備代號:$(getprop ro.product.device)"
+echo "-型號:$(getprop ro.product.model)"
+echo "-Android版本:$(getprop ro.build.version.release)"
+echo "-SDK:$(getprop ro.build.version.sdk)"
+endtime() {
+	#計算總體切換時長耗費
+	case $1 in
+	1) starttime=$starttime1 ;;
+	2) starttime=$starttime2 ;;
+	esac
+	endtime=$(date "+%Y-%m-%d %H:%M:%S")
+	duration=$(echo $(($(date +%s -d "${endtime}") - $(date +%s -d "${starttime}"))) | awk '{t=split("60 秒 60 分 24 時 999 天",a);for(n=1;n<t;n+=2){if($1==0)break;s=$1%a[n]a[n+1]s;$1=int($1/a[n])}print s}')
+	[[ -n $duration ]] && echoRgb "$2用時:$duration" || echoRgb "$2用時:0秒"
+}
+echoRgb() {
+	#轉換echo顏色提高可讀性
+	if [[ -n $2 ]]; then
+		if [[ $3 = 1 ]]; then
+			echo -e "\e[1;32m $1\e[0m"
+		else
+			echo -e "\e[1;31m $1\e[0m"
+		fi
+	else
+		echo -e "\e[1;${bn}m $1\e[0m"
+	fi
+}
+Package_names() {
+	[[ -n $1 ]] && t1="$1"
+	t2=$(appinfo -o pn -pn $t1 2>/dev/null | head -1)
+	[[ -n $t2 ]] && [[ $t2 = $1 ]] && echo $t2
+}
