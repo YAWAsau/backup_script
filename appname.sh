68f8a4e628a03bd4a90901911ac4553df618b2fd
YAWAsau <62833322+YAWAsau@users.noreply.github.com>
2021-08-30 07:07:58 +0800

    Add files via upload

--- appname.sh
+++ appname.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 [[ $(id -u) -ne 0 ]] && echo " 你是憨批？不給Root用你媽 爬" && exit 1
-[[ -z $(echo "$MODDIR" | grep -v 'mt') ]] && echo " 草泥馬不解壓縮？用毛線 憨批" && exit 1
+[[ -z $(echo "$MODDIR" | grep -v 'mt') ]] && echo "我他媽骨灰給你揚了撒了TM不解壓縮？用毛線 憨批" && exit 1
 [[ ! -d $MODDIR/tools ]] && echo " $MODDIR/tools目錄遺失" && exit 1
 #鏈接腳本設置環境變量
 md5path="$MODDIR"
@@ -30,17 +30,17 @@ appinfo -d " " -o ands,pn -pn "$system" -3 2>/dev/null | sort | while read name;
 	echoRgb "$i.$name"
 	if [[ $1 = twrp ]]; then
 		apkpath=$(pm path "$app_2" | cut -f2 -d ':' | head -1)
-		nametxt=$MODDIR/recovery/script/Apkname.txt
-		[[ ! -e $nametxt ]] && echo "#不需要恢復還原的應用請在開頭注釋# 比如#xxxxxxxx 酷安" >"$nametxt"
+		nametxt=$MODDIR/recovery.txt
+		[[ ! -e $nametxt ]] && echo "#不需要備份的應用請在開頭注釋# 比如#xxxxxxxx 酷安" >"$nametxt"
 		[[ -z $(cat "$nametxt" | grep -v "#" | sed -e '/^$/d' | grep -w "$name") ]] && echo "$name ${apkpath%/*}" >>"$nametxt"
 	else
 		nametxt=$txtpath/Apkname.txt
-		[[ ! -e $nametxt ]] && echo "#不需要恢復還原的應用請在開頭注釋# 比如#xxxxxxxx 酷安" >"$nametxt"
+		[[ ! -e $nametxt ]] && echo "#不需要備份的應用請在開頭注釋# 比如#xxxxxxxx 酷安" >"$nametxt"
 		[[ -z $(cat "$nametxt" | grep -v "#" | sed -e '/^$/d' | grep -w "$name") ]] && echo "$name" >>"$nametxt"
 	fi
 	let i++
 	let bn++
 done
 endtime 1
-[[ $1 = twrp ]] && echoRgb " 輸出包名結束 請編輯$MODDIR/recovery/script/Apkname.txt確認無法開機時需要備份應用" || echo " 輸出包名結束 請查看$txtpath/Apkname.txt"
+[[ $1 = twrp ]] && echoRgb " 輸出包名結束 請編輯$MODDIR/recovery.txt確認無法開機時需要備份應用" || echoRgb " 輸出包名結束 請查看$txtpath/Apkname.txt"
 exit 0
--- recovery備份包名生成.sh
+++ recovery備份包名生成.sh
@@ -1,6 +1,6 @@
 MODDIR=${0%/*}
 [[ $(id -u) -ne 0 ]] && echo " 你是憨批？不給Root用你媽 爬" && exit 1
-[[ -z $(echo "$MODDIR" | grep -v 'mt') ]] && echo " 草泥馬不解壓縮？用毛線 憨批" && exit 1
+[[ -z $(echo "$MODDIR" | grep -v 'mt') ]] && echo "我他媽骨灰給你揚了撒了TM不解壓縮？用毛線 憨批" && exit 1
 if [[ -e $MODDIR/appname.sh ]]; then
 	sh "$MODDIR/appname.sh" twrp
 	if [[ ! -d $MODDIR/recovery/tools ]]; then
@@ -11,6 +11,5 @@ if [[ -e $MODDIR/appname.sh ]]; then
 	fi
 else
 	echoRgb "$MODDIR/appname.sh遺失"
-	exit
 fi
 exit 0
